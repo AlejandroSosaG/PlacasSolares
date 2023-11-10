e0bd5e0518d82545a4589d9edc8aa36c77ccfe20
AlejandroSosaG <93648834+AlejandroSosaG@users.noreply.github.com>
2023-11-10 11:52:39 +0100

    Falta fotos

--- PlacasSolares/Views/DetallesPage.xaml.cs
+++ PlacasSolares/Views/DetallesPage.xaml.cs
@@ -1,3 +1,6 @@
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using Microsoft.Maui.Controls;
 using System.Threading.Tasks;
 
 namespace PlacasSolares.Views;
@@ -15,9 +18,35 @@ public partial class DetallesPage : ContentPage
     private async void Guardar(object sender, EventArgs e)
 	{
         CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-        //await Toast.Make("Howdy, I'm a Toast!",
-        //          ToastDuration.Long,
-        //          16)
-        //    .Show(cancellationTokenSource.Token);
+        var toast = Toast.Make("Datos guardados correctamente", ToastDuration.Long, textSize: 32);
+        await toast.Show(cancellationTokenSource.Token);
+    }
+
+    private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        Editor entry = (Editor)sender;
+        string oldText = e.OldTextValue;
+        string newText = e.NewTextValue;
+        string myText = entry.Text;
+    }
+
+    private void Entry_Completed(object sender, EventArgs e)
+    {
+        string text = ((Editor)sender).Text;    
+    }
+
+    private async void Mapa(object sender, TappedEventArgs e)
+    {
+        var location = new Location(47.645160, -122.1306032);
+        var options = new MapLaunchOptions { Name = "Microsoft Building 25" };
+
+        try
+        {
+            await Map.Default.OpenAsync(location, options);
+        }
+        catch (Exception ex)
+        {
+            // No map application available to open
+        }
     }
 }
